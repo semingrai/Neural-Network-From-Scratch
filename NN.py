b52a8aa4830cd56ccc5638d368b8bcdb67935aec
Semin_Rwai <hwangchei901@gmail.com>
2026-03-03 12:53:23 -0500

    ReLU and Softmax

--- NN.py
+++ NN.py
@@ -3,13 +3,40 @@ from nnfs.datasets import spiral_data
 import nnfs
 nnfs.init()
 
+# Neuron creating class
 class Dense_Layer:
     def __init__(self, n_inputs, n_neurons):
         self.weight = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros(1, n_neurons)
-
+        self.biases = np.zeros((1, n_neurons))
+    # Forwarding the input values towards the hidden layers
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weight) + self.biases
 
-class Activation_Relu:
-    
+# ReLU activation function class(0-x)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+# Softmax function for output measures and probablity
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs-np.max(inputs, axis=1, keepdims=True))
+        probablities =  exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probablities
+
+X, y = spiral_data(samples=100, classes=3)
+
+# 2 inputs 1 3 neuron hidden layers
+dense1 =  Dense_Layer(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Dense_Layer(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
